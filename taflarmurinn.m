7aff483cc52452cbfc8a121766d0c4674037bb38
Arnar Þór <arnargisla@gmail.com>
2016-04-07 12:31:19 +0000

    added some servo demo stuff

--- taflarmurinn.m
+++ taflarmurinn.m
@@ -4,23 +4,36 @@ clear a ledPin ledState blinkIntervalInSeconds serialCommunicationPort;
 
 %Configuration
 ledPin = 'D13';
+servoPin = 'D4';
+xPin = 'A1';
+yPin = 'A0';
 serialCommunicationPort = '/dev/ttyS101'; % NOTE in Windows this is 
-                                 ...something like 'COM1' or 'COM23'
+                                 ...something like 'COM1' or 'COM23'     
 blinkIntervalInSeconds = 1;
 
 %Setup
 a = arduino(serialCommunicationPort, 'uno');
 configurePin(a, ledPin, 'DigitalOutput');
+configurePin(a, xPin, 'AnalogInput');
+configurePin(a, yPin, 'AnalogInput');
 
 %Program
 ledState = true;
 writeDigitalPin(a, ledPin, ledState);
+
+clear s;
+s = servo(a, 'D4', 'MinPulseDuration', 850*10^-6, 'MaxPulseDuration', 3850*10^-6);
+
 tic
-while (true)
-    if(toc > blinkIntervalInSeconds)
+while(true)
+    writePosition(s, readVoltage(a, xPin)/5);
+    if(toc > 0.2)
         tic
-        ledState = not (ledState);
-        writeDigitalPin(a, ledPin, ledState);
-        disp 'blink'
+        readVoltage(a, xPin)
     end
 end
+
+writePosition(s, 0/180);
+pause(1);
+
+
