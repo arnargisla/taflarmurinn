0eda6103d58c9d3fc35692400b1ebfe5eee08b88
Arnar Þór <arnargisla@gmail.com>
2016-04-12 10:28:26 +0000

    change

--- taflarmuruppkast.m
+++ taflarmuruppkast.m
@@ -1,21 +1,29 @@
-clear;
-refresh;
+figure
 
 % Define the links using Denavit-Hartenberg (D-H) parameters
-L(1) = Link('alpha',    0, 'a',    0, 'd',   0);
-L(2) = Link('alpha', pi/2, 'a',    0, 'd', 0.2);
-L(3) = Link('alpha',    0, 'a', 0.30, 'd',   0);
-L(4) = Link('alpha',    0, 'a', 0.30, 'd',   0);
-L(5) = Link('alpha', pi/2, 'a', 0.30, 'd',   0);
-L(6) = Link('alpha', pi/2, 'a', 0.30, 'd',   0);
+L(1) = Link('alpha', pi/2, 'a',    0, 'd', 0.10);
+L(2) = Link('alpha',    0, 'a',  0.25, 'd',   0);
+L(3) = Link('alpha',    0, 'a',  0.20, 'd',   0);
+L(4) = Link('alpha', -pi/2, 'a', 0, 'd',    0);
+L(5) = Link('alpha',  pi/2, 'a', 0, 'd',   0.3);
+L(6) = Link('alpha', -pi/2, 'a', 0.3, 'd',   0.0);
+
+
+% Define the links using Denavit-Hartenberg (D-H) parameters
+L(1) = Link('alpha', pi/2, 'a',    0, 'd', 0.10);
+L(2) = Link('alpha',    0, 'a',  0.25, 'd',   0);
+L(3) = Link('alpha',    0, 'a',  0.20, 'd',   0);
+L(4) = Link('alpha', -pi/2, 'a', 0.5, 'd',    0);
+L(5) = Link('alpha',  pi/2, 'a', 0, 'd',   0.3);
+L(6) = Link('alpha', -pi/2, 'a', 0.3, 'd',   0.0);
 
 % Set joint limits
 %L(1) does not have any limits
-L(2).qlim = pi/180*[-90 90];
-L(3).qlim = pi/180*[-90 90];
-L(4).qlim = pi/180*[-90 90];
-L(5).qlim = pi/180*[-90 90];
-L(6).qlim = pi/180*[-90 90];
+%L(2).qlim = pi/180*[-90 90];
+%L(3).qlim = pi/180*[-90 90];
+%L(4).qlim = pi/180*[-90 90];
+%L(5).qlim = pi/180*[-90 90];
+%L(6).qlim = pi/180*[-90 90];
 
 % Define the robot, composed of the links
 MyRobot = SerialLink(L);
@@ -40,24 +48,24 @@ qf = [pi/4 0 pi/4 0 0 0];
 [q, qd, qdd] = jtraj(qi, qf, t);
 
 % Animate end effector trajectory in 3D space
-MyRobot.plot(q);%, 'workspace', [-22 38 -22 38 -22 38]);
+%MyRobot.plot(q);%, 'workspace', [-22 38 -22 38 -22 38]);
 %plot(MyRobot, q);
-hold;
+%hold;
 
 
-qi = qf;
-qf = [pi/4 pi/2 pi/4 pi/4 pi/4 pi/4];
-[q, qd, qdd] = jtraj(qi, qf, t);
-MyRobot.plot(q);
+%qi = qf;
+%qf = [pi/4 pi/2 pi/4 pi/4 pi/4 pi/4];
+%[q, qd, qdd] = jtraj(qi, qf, t);
+%MyRobot.plot(q);
 
-qi = qf;
-qf = [pi/4 pi/4 pi/4 pi/4 pi/4 pi/4];
-[q, qd, qdd] = jtraj(qi, qf, t);
-plot = MyRobot.plot(q);
-
-
+%qi = qf;
+%qf = [pi/4 pi/4 pi/4 pi/4 pi/4 pi/4];
+%[q, qd, qdd] = jtraj(qi, qf, t);
+%plot = MyRobot.plot(q);
 
 
 
-qf = [pi/2 0 pi/4 pi/4 pi/4 pi/4];
-MyRobot.plot(qf);
+
+
+qf = [0 0 0 0 0 0];
+MyRobot.teach();
